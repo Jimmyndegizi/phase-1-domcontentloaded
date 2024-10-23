a0813dbcd015c5e53b449b15cd7ae8c84a314910
jimmyndegizi <ndegwajames123@gmail.com>
2024-10-23 19:19:33 +0300

    learnt about addEventListener

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', () => {
+    const textElement = document.getElementById('text');
+
+    textElement.textContent = "JavaScript is so cool. It lets me add text to my page programmatically!";
+})
